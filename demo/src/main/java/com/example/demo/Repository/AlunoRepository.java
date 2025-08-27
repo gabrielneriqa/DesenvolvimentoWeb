892374457323fdaac0c6710261153e778b14e242
ALUNO C26 <alunoc26@unifor.local>
2025-08-26 22:03:24 -0300

    Aula Terça feira

---
+++ demo/src/main/java/com/example/demo/Repository/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.Repository;
+
+import com.example.demo.entity.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno,Long> {
+
+    ;
+}
---
+++ demo/src/main/java/com/example/demo/Service/AlunoService.java
@@ -0,0 +1,42 @@
+package com.example.demo.Service;
+
+import com.example.demo.Repository.AlunoRepository;
+import com.example.demo.entity.Aluno;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AlunoService {
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    private List<Aluno> findAllAlunos() {
+        return alunoRepository.findAll();
+    }
+
+    private Optional<Aluno> findAlunoById(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    private Aluno createAluno(Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    private void deleteAluno(Long id) {
+        alunoRepository.deleteById(id);
+    }
+
+    private Aluno updateAluno(Long id, Aluno updateAluno) {
+        return alunoRepository.findById(id).map(aluno -> {
+            aluno.setNome(updateAluno.getNome());
+            aluno.setEmail(updateAluno.getEmail());
+            aluno.setDataNasc(updateAluno.getDataNasc());
+            aluno.setSenha(updateAluno.getSenha());
+            return alunoRepository.save(aluno);
+        }).orElseThrow(() -> new RuntimeException("Tem esse aluno aí não mah!"));
+    }
+
+}
